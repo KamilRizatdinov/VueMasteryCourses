5e9a7c079717662be1a1b2d201a697d83c6d5445
KamilRizatdinov <k.rizatdinov@innopolis.university>
2020-04-16 21:07:26 +0300

    Add list of events to EventList

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,5 +1,14 @@
 <template>
-  <h1>Events listing</h1>
+  <div>
+    <h1>Listing events</h1>
+    <ul>
+      <li v-for="i in 10" :key="i">
+        <router-link :to="{ name: 'event-show', params: { id: i } }"
+          >Show event</router-link
+        >
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
