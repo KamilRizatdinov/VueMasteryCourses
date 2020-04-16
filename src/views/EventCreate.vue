6905a72e057fd904a1287e67cadbff7ac00c3ae5
KamilRizatdinov <k.rizatdinov@innopolis.university>
2020-04-16 20:15:55 +0300

    Create own page components

---
+++ src/views/EventCreate.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Create your event here!</h1>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ src/views/EventList.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>Events listing</h1>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
