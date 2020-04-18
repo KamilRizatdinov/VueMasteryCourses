19e9ad4f01627e9bff088d4106ba81b0d788f870
KamilRizatdinov <k.rizatdinov@innopolis.university>
2020-04-18 16:48:14 +0300

    Add routing to the new created event

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -70,8 +70,10 @@ export default {
       const user = this.$store.state.user;
       const id = Math.floor(Math.random() * 10000000);
       return {
+        id: id,
+        user: user,
         category: "",
-        organizer: user,
+        organizer: user.name,
         title: "",
         description: "",
         location: "",
